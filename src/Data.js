b7a32745a3e06c14eff006a0417e08b3974f90d1
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-04-27 15:20:58 +0600

    sent data to server

--- src/Data.js
+++ src/Data.js
@@ -9,8 +9,35 @@ const Data = () => {
     }, []);
 
 
+    const handleAddUser = event=>{
+        event.preventDefault();
+       const name = event.target.name.value;
+       const email = event.target.email.value;
+       const user = {name, email};
+        console.log(name, email);
+        //send data/ post data// 
+        fetch('http://localhost:5000/user', {
+            method : 'POST',
+            headers : {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data=>{
+            console.log(data);
+        })
+    }
+
     return (
         <div>
+            <div>
+                <form onSubmit={handleAddUser}>
+                    <input type="text" name='name' placeholder='name' required />
+                    <input type="text" name='email' placeholder='email' required/>
+                    <input type="submit" value='Add User' />
+                </form>
+            </div>
             <h1>My Own Data: {users.length} </h1>
           <div>
               {
